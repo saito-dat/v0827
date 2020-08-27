64bed8db397b7519360f667623d63fbccb7515dc
Fukata <satoshidat@gmail.com>
2020-08-27 12:12:11 +0900

    12-2

--- v0827/Form1.Designer.cs
+++ v0827/Form1.Designer.cs
@@ -42,7 +42,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(192, 202);
+            this.label1.Location = new System.Drawing.Point(53, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 0;
@@ -51,7 +51,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(79, 103);
+            this.label2.Location = new System.Drawing.Point(12, 32);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 1;
@@ -62,7 +62,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(138, 123);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -12,21 +12,62 @@ namespace v0827
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
+        int vx = rand.Next(-10,11);
+        int vy = rand.Next(-10,11);
+        int point = 100;
+        static Random rand = new Random();
 
         public Form1()
         {
             InitializeComponent();
+
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Height - label1.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+
             label1.Left += vx;
             label1.Top += vy;
 
+            //2次元クラスPoint型の変数mpを宣言
             Point mp = MousePosition;
+
+            // ラベルに座標を表示
+            //// 変換したフォーム座標は、mp.X、mp.Yで取り出せる。
             label2.Text = "" + mp.X + "," + mp.Y;
+
+            if (label1.Left < 0) 
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if(label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if(label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+
+            // cposに、マウスのフォーム座標を取り出す。
+            //// MousePositionにマウス座標のスクリーン左上からのX、Yが入っている。
+            //// PointToClient()を使うと、スクリーン座標を、フォーム座標に変換できる
+            mp = PointToClient(mp);
+
+            if (  (mp.X >= label1.Left)
+               && (mp.X <  label1.Right)
+               && (mp.Y >= label1.Top)
+               && (mp.Y <  label1.Bottom)  
+               )
+            {
+                timer1.Enabled = false;
+            }
         }
 
         private void label2_Click(object sender, EventArgs e)
